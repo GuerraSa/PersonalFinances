ece4c7f398499714f55b38cec06416c4b561452f
GuerraSa <31255580+GuerraSa@users.noreply.github.com>
2023-11-01 09:41:53 -0700

    Uplaod CC Statement script

---
+++ R/CC Statement TS.R
@@ -0,0 +1,10 @@
+library(data.table)
+library(tidyverse)
+source("src/functions.R")
+
+statement <- load_statement(cc = "AMEX") %>% 
+  .[!str_detect(Merchant, "INSTALLMENT PLAN") | Amount < 0, Balance := cumsum(Amount)]
+
+ggplot(statement[!is.na(Balance)]) +
+  geom_line(aes(x = Date, y = Balance)) +
+  theme_minimal()
